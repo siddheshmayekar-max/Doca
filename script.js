3ec524ea56720b8ad29dae4bf0a8fefe2dcc896b
Siddhesh Mayekar <siddhesh08mayekar@gamil.com>
2026-02-01 21:42:08 +0530

    01 feb section1 development

---
+++ script.js
@@ -0,0 +1,10 @@
+// Sticky Navbar on Scroll
+const navbar = document.querySelector(".navbar-custom");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
